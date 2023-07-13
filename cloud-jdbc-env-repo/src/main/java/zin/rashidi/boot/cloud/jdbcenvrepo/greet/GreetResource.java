f94942fd3e30f3cf6e0c03ea5632dbe66fe8fc40
Rashidi Zin <rashidi@zin.my>
2023-07-13 09:40:37 +0800

    Clean up

---
+++ cloud-jdbc-env-repo/src/main/java/zin/rashidi/boot/cloud/jdbcenvrepo/greet/GreetResource.java
@@ -0,0 +1,24 @@
+package zin.rashidi.boot.cloud.jdbcenvrepo.greet;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Rashidi Zin, GfK
+ */
+@RestController
+class GreetResource {
+
+    private final GreetProperties properties;
+
+    GreetResource(GreetProperties properties) {
+        this.properties = properties;
+    }
+
+    @GetMapping("/greet")
+    public String greet(@RequestParam String greeting) {
+        return String.format("%s, my name is %s", greeting, properties.name());
+    }
+
+}
