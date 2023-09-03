c4df856b2364bb77f1650a914e847fbe0ea62ad9
Rashidi Zin <rashidi@zin.my>
2023-09-03 23:54:07 +0800

    Initial commit

---
+++ batch-rest-repository/src/test/java/zin/rashidi/boot/batch/BatchRestRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package zin.rashidi.boot.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchRestRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
