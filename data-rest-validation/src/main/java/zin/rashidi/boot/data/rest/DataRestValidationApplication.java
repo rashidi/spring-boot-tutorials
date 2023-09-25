56de9326ad99c37dc83c0dc40303c40e0fd08bc4
Rashidi Zin <rashidi@zin.my>
2023-09-25 18:47:28 +0800

    Migrate graphql

---
+++ data-rest-validation/src/main/java/zin/rashidi/boot/data/rest/DataRestValidationApplication.java
@@ -0,0 +1,13 @@
+package zin.rashidi.boot.data.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataRestValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataRestValidationApplication.class, args);
+	}
+
+}
