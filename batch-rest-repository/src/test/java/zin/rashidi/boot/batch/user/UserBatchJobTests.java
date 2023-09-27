f4c12cfef7a30bcc23dec2ff441cd08598165eb5
Rashidi Zin <rashidi@zin.my>
2023-09-27 16:59:37 +0800

    Convert batch-rest-repository test to slice test

--- batch-rest-repository/src/test/java/zin/rashidi/boot/batch/user/UserBatchJobTests.java
+++ batch-rest-repository/src/test/java/zin/rashidi/boot/batch/user/UserBatchJobTests.java
@@ -2,36 +2,106 @@ package zin.rashidi.boot.batch.user;
 
 import static java.time.Duration.ofSeconds;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.batch.core.ExitStatus.COMPLETED;
 import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
+import static zin.rashidi.boot.batch.user.UserBatchJobTests.BatchTestConfiguration;
+import static zin.rashidi.boot.batch.user.UserBatchJobTests.MongoTestConfiguration;
+
+import javax.sql.DataSource;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-import zin.rashidi.boot.batch.TestBatchRestRepositoryApplication;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
 
 /**
  * @author Rashidi Zin
  */
-@Import(TestBatchRestRepositoryApplication.class)
-@SpringBootTest(properties = "spring.batch.jdbc.initialize-schema=always")
+@Testcontainers
+@SpringBatchTest
+@SpringJUnitConfig({ BatchTestConfiguration.class, MongoTestConfiguration.class, UserJobConfiguration.class })
 class UserBatchJobTests {
 
+    @Container
+    @ServiceConnection
+    private final static MySQLContainer<?> MYSQL_CONTAINER = new MySQLContainer<>("mysql:latest")
+            .withInitScript("org/springframework/batch/core/schema-mysql.sql");
+
+    @Container
+    @ServiceConnection
+    private final static MongoDBContainer MONGO_DB_CONTAINER = new MongoDBContainer("mongo:latest");
+
     @Autowired
-    private MongoOperations mongo;
+    private JobLauncherTestUtils launcher;
+
+    @Autowired
+    private MongoOperations mongoOperations;
 
     @Test
-    @DisplayName("Given there are 10 users returned from REST Service When the job is executed Then all users should be saved to MongoDB")
+    @DisplayName("Given there are 10 users returned from REST Service When the job is COMPLETED Then all users should be saved to MongoDB")
     void launch() {
 
         await().atMost(ofSeconds(30)).untilAsserted(() -> {
-            var persistedUsers = mongo.findAll(User.class);
+            var execution = launcher.launchJob();
 
-            assertThat(persistedUsers).hasSize(10);
+            assertThat(execution.getExitStatus()).isEqualTo(COMPLETED);
         });
+
+        var persistedUsers = mongoOperations.findAll(User.class);
+
+        assertThat(persistedUsers).hasSize(10);
+    }
+
+    @TestConfiguration
+    static class BatchTestConfiguration extends DefaultBatchConfiguration {
+
+        @Override
+        protected DataSource getDataSource() {
+            return DataSourceBuilder.create()
+                    .url(MYSQL_CONTAINER.getJdbcUrl())
+                    .username(MYSQL_CONTAINER.getUsername())
+                    .password(MYSQL_CONTAINER.getPassword())
+                    .build();
+        }
+
+        @Override
+        @Bean
+        protected PlatformTransactionManager getTransactionManager() {
+            return new JdbcTransactionManager(getDataSource());
+        }
+
+    }
+
+    @TestConfiguration
+    static class MongoTestConfiguration extends AbstractMongoClientConfiguration {
+
+        @Override
+        protected String getDatabaseName() {
+            return "test";
+        }
+
+        @Override
+        protected void configureClientSettings(MongoClientSettings.Builder builder) {
+            builder.applyConnectionString(new ConnectionString(MONGO_DB_CONTAINER.getReplicaSetUrl()));
+        }
 
     }
 
