2151347fddc54f8583f776993c0841ee5e5f0fa5
Rashidi Zin <rashidi@zin.my>
2023-11-07 18:58:53 +0800

    Initial

---
+++ jooq/src/test/java/zin/rashidi/boot/jooq/JooqApplicationTests.java
@@ -0,0 +1,13 @@
+package zin.rashidi.boot.jooq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
