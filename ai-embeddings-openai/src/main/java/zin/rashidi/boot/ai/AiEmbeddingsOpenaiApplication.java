c4ae51db77465ecd6e9fdd9a0da1dbe72bbafd96
Rashidi Zin <rashidi@zin.my>
2024-03-03 10:19:11 +0800

    Create new Spring Boot project 'ai-embeddings-openai'
    A fresh Spring Boot project named 'ai-embeddings-openai' has been created. It includes essential configuration files such as .gitignore, build.gradle, settings.gradle and application.properties. Also added initial application and test class skeletons. Project has been included into the main settings.gradle as well.

---
+++ ai-embeddings-openai/src/main/java/zin/rashidi/boot/ai/AiEmbeddingsOpenaiApplication.java
@@ -0,0 +1,13 @@
+package zin.rashidi.boot.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiEmbeddingsOpenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiEmbeddingsOpenaiApplication.class, args);
+	}
+
+}
