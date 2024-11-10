542aac7bdf921c77dc7214f4b303f6c2bd6b04d0
Rashidi Zin <rashidi@zin.my>
2024-11-10 13:45:31 +0800

    Add example for implementing validation with Spring Data JPA with events

---
+++ data-jpa-event/src/main/java/zin/rashidi/data/event/user/UserValidation.java
@@ -0,0 +1,28 @@
+package zin.rashidi.data.event.user;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Rashidi Zin
+ */
+@Component
+class UserValidation {
+
+    private final UserRepository repository;
+
+    UserValidation(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @EventListener
+    void usernameIsUnique(UserBeforeSaveEvent event) {
+        var usernameExisted = repository.existsByUsername(event.getSource().getUsername());
+
+        if (usernameExisted) {
+            throw new IllegalArgumentException("Username is already taken");
+        }
+
+    }
+
+}
