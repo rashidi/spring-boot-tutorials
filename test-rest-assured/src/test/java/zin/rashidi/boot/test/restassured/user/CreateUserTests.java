742c43cad2911673d8ab8ae1c2d0fbb0948d41d2
Rashidi Zin <rashidi@zin.my>
2024-11-16 14:29:04 +0800

    Fix test-rest-assured

--- test-rest-assured/src/test/java/zin/rashidi/boot/test/restassured/user/CreateUserTests.java
+++ test-rest-assured/src/test/java/zin/rashidi/boot/test/restassured/user/CreateUserTests.java
@@ -7,32 +7,30 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.TestExecutionListeners;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
-import static java.time.Duration.ofMinutes;
 import static org.apache.http.HttpStatus.SC_BAD_REQUEST;
 import static org.apache.http.HttpStatus.SC_CREATED;
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static org.testcontainers.utility.MountableFile.forClasspathResource;
+import static org.springframework.test.context.TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS;
 
 /**
  * @author Rashidi Zin
  */
 @Testcontainers
+@TestExecutionListeners(listeners = UserCreationTestExecutionListener.class, mergeMode = MERGE_WITH_DEFAULTS)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 class CreateUserTests {
 
     @Container
     @ServiceConnection
-    private static final MongoDBContainer mongo = new MongoDBContainer("mongo:latest")
-            .withCopyToContainer(forClasspathResource("mongo-init.js"), "/docker-entrypoint-initdb.d/mongo-init.js")
-            .withStartupAttempts(2)
-            .withStartupTimeout(ofMinutes(10));
+    private static final MongoDBContainer mongo = new MongoDBContainer("mongo:latest");
 
     @BeforeAll
     static void port(@LocalServerPort int port) {
--- test-rest-assured/src/test/resources/mongo-init.js
+++
@@ -1,6 +0,0 @@
-db.createCollection("user");
-
-db.user.insert({
-  "name": "Zaid Zin",
-  "username": "zaid.zin"
-});
