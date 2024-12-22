be5ffd29bbda9b2bdd2712d41fb1ece0bf2b71be
Rashidi Zin <rashidi@zin.my>
2024-12-22 13:25:14 +0800

    Polish

--- test-slice-tests-rest/src/main/java/zin/rashidi/boot/test/slices/security/WebSecurityConfiguration.java
+++ test-slice-tests-rest/src/main/java/zin/rashidi/boot/test/slices/security/WebSecurityConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -19,7 +18,6 @@ class WebSecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(withDefaults())
                 .authorizeHttpRequests(requests -> requests.anyRequest().authenticated())
                 .build();
