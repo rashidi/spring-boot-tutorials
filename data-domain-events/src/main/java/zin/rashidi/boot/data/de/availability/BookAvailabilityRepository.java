72c7f8ed47e224aa7d014b0d0d3f670a6628a6e4
Rashidi Zin <rashidi@zin.my>
2025-03-22 07:31:04 +0800

    Undo changes

--- data-domain-events/src/main/java/zin/rashidi/boot/data/de/availability/BookAvailabilityRepository.java
+++ data-domain-events/src/main/java/zin/rashidi/boot/data/de/availability/BookAvailabilityRepository.java
@@ -1,8 +1,8 @@
 package zin.rashidi.boot.data.de.availability;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * @author Rashidi Zin
