27abae3d6a55e7162a9daf86e1c0234142979c6e
Rashidi Zin <rashidi@zin.my>
2025-05-18 10:28:17 +0800

    Migrate gradle configuration to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,64 @@
+import org.springframework.boot.gradle.plugin.SpringBootPlugin
+
+plugins {
+    java
+    id("org.springframework.boot") version "3.4.5" apply false
+    id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "6.2.0.5505"
+    jacoco
+}
+
+group = "zin.rashidi.boot"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    subprojects.forEach { p ->
+        implementation(project(":${p.name}"))
+    }
+}
+
+dependencies {
+    implementation(platform(SpringBootPlugin.BOM_COORDINATES))
+}
+
+// Simple JaCoCo report task
+val reportTask = tasks.register<JacocoReport>("testCodeCoverageReport") {
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "rashidi_spring-boot-tutorials")
+        property("sonar.organization", "rashidi-github")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.coverage.jacoco.xmlReportPaths", "${layout.buildDirectory.get()}/reports/jacoco/testCodeCoverageReport/testCodeCoverageReport.xml")
+    }
+}
+
+subprojects {
+    apply(plugin = "jacoco")
+
+    // Only configure the test task if it exists
+    tasks.matching { it.name == "test" }.configureEach {
+        if (this is Test) {
+            finalizedBy(tasks.matching { it.name == "jacocoTestReport" })
+        }
+    }
+}
+
+tasks.named("check") {
+    dependsOn(reportTask)
+}
---
+++ data-jdbc-schema-generation/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "data-jdbc-schema-generation"
