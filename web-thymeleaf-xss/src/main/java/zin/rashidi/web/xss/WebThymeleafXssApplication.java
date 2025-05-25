8c561bf0633d44e40a28996e17e70a684c9ac103
Rashidi Zin <rashidi@zin.my>
2025-05-25 15:15:33 +0800

    Add XSS protection example (#238)

---
+++ web-thymeleaf-xss/src/main/java/zin/rashidi/web/xss/WebThymeleafXssApplication.java
@@ -0,0 +1,13 @@
+package zin.rashidi.web.xss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebThymeleafXssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebThymeleafXssApplication.class, args);
+    }
+
+}
---
+++ web-thymeleaf-xss/src/test/java/zin/rashidi/web/xss/greet/GreetResourceTests.java
@@ -0,0 +1,34 @@
+package zin.rashidi.web.xss.greet;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author Rashidi Zin
+ */
+@WebMvcTest(controllers = GreetResource.class, includeFilters = @Filter(classes = EnableWebSecurity.class))
+class GreetResourceTests {
+
+    @Autowired
+    private MockMvcTester mvc;
+
+    @Test
+    @DisplayName("Given XSS protection is enabled Then response header should contain information about X-XSS-Protection and Content-Security-Policy")
+    void headers() {
+        mvc.get()
+                .uri("/greet?name={name}", "rashidi")
+            .assertThat()
+                .matches(status().isOk())
+                .matches(header().string("Content-Security-Policy", "default-src 'self'"))
+                .matches(header().string("X-XSS-Protection", "1; mode=block"));
+    }
+
+}
