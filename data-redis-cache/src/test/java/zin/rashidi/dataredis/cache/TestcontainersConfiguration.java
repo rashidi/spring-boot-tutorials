0ccd64b5f6abeeb441622197faf07ac13488fcf0
Rashidi Zin <rashidi@zin.my>
2025-07-20 20:30:11 +0800

    Add caching example with Spring Data Redis

---
+++ data-redis-cache/src/test/java/zin/rashidi/dataredis/cache/TestcontainersConfiguration.java
@@ -0,0 +1,25 @@
+package zin.rashidi.dataredis.cache;
+
+import com.redis.testcontainers.RedisContainer;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestcontainersConfiguration {
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	}
+
+	@Bean
+	@ServiceConnection(name = "redis")
+	RedisContainer redisContainer() {
+		return new RedisContainer(DockerImageName.parse("redis:latest"));
+	}
+
+}
