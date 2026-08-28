255abacc23b30ae22e93b6ec6f1e884638d1d079
Rashidi Zin <rashidi@zin.my>
2026-08-29 00:51:55 +0800

    Add example implementing optimistic locking with Spring Data JDBC

---
+++ data-jdbc-optimistic-locking/src/main/java/zin/rashidi/boot/data/jdbc/user/User.java
@@ -0,0 +1,32 @@
+package zin.rashidi.boot.data.jdbc.user;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.relational.core.mapping.Table;
+
+/**
+ * @author Rashidi Zin
+ */
+@Table("users")
+class User {
+
+    @Id
+    private Long id;
+
+    @Version
+    private Long version;
+
+    private final String name;
+    private String username;
+
+    User(String name, String username) {
+        this.name = name;
+        this.username = username;
+    }
+
+    public User username(String username) {
+        this.username = username;
+        return this;
+    }
+
+}
